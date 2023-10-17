48ea704b824ec0f2d1523d62e60ea75651ef18d8
yyss99 <yyssjj9999@gmail.com>
2023-10-17 17:21:16 +0900

    🩹chore: 주석 삭제

--- weather/weather/SceneDelegate.swift
+++ weather/weather/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController() // 시작 VC 작성해주기
+        window.rootViewController = ViewController() 
         window.makeKeyAndVisible()
         self.window = window
     }
